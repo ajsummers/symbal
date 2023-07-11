81bd47fdc1ab747935c0bad95071cd8b45a85404
ajsummers <ajs0201@auburn.edu>
2023-07-11 17:22:30 -0500

    Finished base case

--- symbal/__init__.py
+++ symbal/__init__.py
@@ -6,7 +6,8 @@ import sys
 if sys.version_info[0] == 2:
     raise ImportError('SymBAL requires Python 3.7. You have Python 2 installed.')
 
-__all__ = ['TestFunction']
-__version__ = '0.0.2'
+__all__ = ['TestFunction', 'SymbalTest']
+__version__ = '0.0.3'
 
 from symbal.test_function import TestFunction
+from symbal.main import SymbalTest
